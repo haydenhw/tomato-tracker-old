6778fb012f17cc6afb6412d0734d3e3a38c7ef85
hayden7913 <hayden321@gmail.com>
2017-03-06 15:03:51 -0800

    mongo server working

--- server.js
+++ server.js
@@ -1,25 +1,64 @@
-const express = require('express');
-const app = express();
+const express = require('express')
 const bp = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
 
-app.set('port', (process.env.PORT || 3001));
+mongoose.Promise = global.Promise;
 
-// Express only serves static assets in production
+const {PORT, DATABASE_URL} = require('./config');
+//app.set('port', (process.env.PORT || 3001));
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
-console.log('hello')
 
 app.use(bp.urlencoded({
   extended: true
 }));
 app.use(bp.json());
+// app.use(express.static(__dirname + '/public'));
 
 app.get('/test', (req, res) => {
-  console.log('hit');
-  res.send({test:'success'});
+  console.log('mongo');
+  res.send({test:'mongo'});
 });
 
-app.listen(app.get('port'), () => {
-  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
-});
+let server;
+
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+     return new Promise((resolve, reject) => {
+       console.log('Closing server');
+       server.close(err => {
+           if (err) {
+               return reject(err);
+           }
+           resolve();
+       });
+     });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
