263c4df520571a6ada37d72086ee77c8f6b81ce0
hayden7913 <hayden321@gmail.com>
2017-03-06 13:52:53 -0800

    fullstack working

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const bp = require('body-parser');
+
+app.set('port', (process.env.PORT || 3001));
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+console.log('hello')
+
+app.use(bp.urlencoded({
+  extended: true
+}));
+app.use(bp.json());
+
+
+app.get('/test', (req, res) => {
+  console.log('hit');
+  res.send({test:'success'});
+});
+
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
