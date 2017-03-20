648296d171b6436d71068a5a48774bcc4ae55213
hayden <hayden321@gmail.com>
2017-03-20 00:14:39 -0700

    module container to class

--- client/src/components/ModuleContainer.js
+++ client/src/components/ModuleContainer.js
@@ -5,26 +5,35 @@ import { connect } from 'react-redux';
 import * as actions from '../actions/indexActions';
 import store from '../store';
 import Module from './Module';
-import { squareData } from '../data'
+import { squareData } from '../data';
 
-function MoudleContainer(props) {
-  const modules = props.modules.map((modules, index) =>{
-    return <Module 
-        key={index}
-        index={index}
-        x={modules.x} 
-        y={modules.y}
-        height={modules.height}
-        width={modules.width}
-        image={modules.image}
-      />
-  });
+class MoudleContainer extends Component{
   
-  return (
-    <Group>
-      {modules}
-    </Group>
-  )
+  checkBoundaries() {
+    console.log('hello')
+  }
+
+  render() {
+    const modules = this.props.modules.map((modules, index) =>{
+      return <Module 
+          key={index}
+          index={index}
+          x={modules.x} 
+          y={modules.y}
+          height={modules.height}
+          width={modules.width}
+          image={modules.image}
+          onDragMove={this.checkBoundaries.bind(this)}
+        />
+    });
+    
+    return (
+      <Group>
+        {modules}
+      </Group>
+    )
+  }
+  
 }
 
 const mapStateToProps = (state) => ({
