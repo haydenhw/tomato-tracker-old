90b9827f70ecd760be6cf12a1f840e9b93bd6403
hayden7913 <hayden321@gmail.com>
2017-04-02 17:43:00 -0700

    monog uri build

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL; 
+                       global.DATABASE_URL ||
+                       'mongodb://hayden321:46869269a@ds119380.mlab.com:19380/bread-pcb-tool';
+                        
               
 exports.PORT = process.env.PORT || 3001;
