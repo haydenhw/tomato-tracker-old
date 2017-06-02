da02d5a5f7dc809d92beee3f955edf042beef567
hayden <hayden321@gmail.com>
2017-06-02 15:00:57 -0700

    added fetch projects to time tracker page

--- client/src/containers/TimeTrackerPage.js
+++ client/src/containers/TimeTrackerPage.js
@@ -6,17 +6,32 @@ export default class TimeTrackerPage extends Component {
     super(props);
     
     this.state = {
-      tasks: []
+      tasks: [],
+      isFetching: false;
     }
   }
   
   componentDidMount() {
+    this.setState({isFetching: true});
     
+    fetch('/projects')
+      .then(res => res.json())
+      .then((data) => {
+        console.log(data)
+        this.setState({
+          tasks: data
+          isFetching: false
+        });
+      })
+      .catch((err) => {
+        this.setState({isFetching: false});
+        console.error(err);
+      });
   }
   
   render() {
     const { tasks } = this.state; 
-    
+    console.log(tasks)
     return (
       <TimeTracker tasks={tasks} />
     )
