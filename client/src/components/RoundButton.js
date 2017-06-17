eba0eaf8dfb0f4c00de348f1f92bbc80e3950aae
hayden7913 <hayden321@gmail.com>
2017-06-16 20:02:06 -0700

    add button styles

--- client/src/components/RoundButton.js
+++ client/src/components/RoundButton.js
@@ -5,19 +5,21 @@ import FontAwesome from 'react-fontawesome';
 export default function RoundButton(props) {
   const { clickHandler, icon } = props;
   return (
-    <div
-      className={'round-button'}
-      onClick={clickHandler}
-      role="button"
-    >
-
-      <div className="round-button-icon"><i className="icon icon-plus"></i></div>
-      <div className="round-button-circle" />
-    </div>
-  );
-}
-
-RoundButton.propTypes = {
-  clickHandler: PropTypes.func.isRequired,
-  icon: PropTypes.object.isRequired,
-};
+    <div className="round-button-wrapper"> 
+      <div
+        className="round-button"
+        onClick={clickHandler}
+        role="button"
+        >
+          <div className="round-button-circle">
+            <div className="round-button-icon"><i className="icon icon-plus"></i></div>
+          </div> 
+        </div>
+      </div>
+    );
+  }
+  
+  RoundButton.propTypes = {
+    clickHandler: PropTypes.func.isRequired,
+    icon: PropTypes.object.isRequired,
+  };
