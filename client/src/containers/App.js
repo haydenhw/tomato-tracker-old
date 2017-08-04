774a79f4c6f604a786c69f005d9c48f703034200
hayden7913 <hayden321@gmail.com>
2017-08-04 11:05:11 -0700

    removing li from nav

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,20 +1,37 @@
+// extract nav presentational component
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { hashHistory, Link  } from 'react-router';
 
+import { routeToProjectsPage } from 'helpers/route';
+
 import { fetchProjects, toggleProjectNagModal } from '../actions/indexActions';
 
 class App extends Component {
+  constructor() {
+    super(); 
+    
+    this.state = {
+      activeLink: 'TIMER'
+    }
+  }
+  
   componentDidMount() {
     const { fetchProjects } = this.props;
     
-    fetchProjects()
+    fetchProjects();
   }
   
   handleTimerLinkClick() {
     const { projects, toggleProjectNagModal } = this.props;
     
     projects.length ? hashHistory.push('/') : toggleProjectNagModal();
+    this.setState({ activeLink: 'TIMER' })
+  }
+  
+  handleProjectsLinkClck() {
+    routeToProjectsPage();
+    this.setState({ activeLink: 'PROJECTS' })
   }
   
   render() {
@@ -23,8 +40,12 @@ class App extends Component {
         <nav>
           <h1 className="logo-text">PomTracker</h1>
           <ul role="nav">
-            <li className="nav-link" onClick={this.handleTimerLinkClick.bind(this)}><a>Timer</a></li>
-            <li className="nav-link"><Link to="/projects">Projects</Link></li>
+            <li className="nav-link" onClick={this.handleTimerLinkClick.bind(this)}>
+              <a className={`${this.state.activeLink === 'TIMER' ? 'active-link' : ''}`}>Timer</a>
+            </li>
+            <li className="nav-link" onClick={this.handleProjectsLinkClck.bind(this)}>
+              <a className={`${this.state.activeLink === 'PROJECTS' ? 'active-link' : ''}`}>Projects</a>
+            </li>
           </ul>
         </nav>
           {this.props.children}
