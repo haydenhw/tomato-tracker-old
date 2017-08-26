1ee03609dff98dbb82dc5c4b0007a4f084c04209
hayden7913 <hayden321@gmail.com>
2017-08-26 16:31:53 -0700

    changing name of editmenu to contextmenu

--- client/src/components/TotalTime.js
+++ client/src/components/TotalTime.js
@@ -6,7 +6,7 @@ export default function TotalTime(props){
   
   return (
     <div className='total-time list-item'>
-      <div className="list-item-col1 list-col total"><span>Total:</span></div>
+      <div className="list-item-col1 list-col total-time-total"><span>Total:</span></div>
       <div className="list-item-col2 list-col"><span>{time}</span></div>
       <div className="list-item-col3 list-col"></div>
     </div>
