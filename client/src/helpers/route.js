1a8e79d92090aa1afc52e815b3385535d6af5628
hayden7913 <hayden321@gmail.com>
2017-09-11 18:35:39 -0700

    build

--- client/src/helpers/route.js
+++ client/src/helpers/route.js
@@ -2,8 +2,10 @@ import { hashHistory } from 'react-router';
 
 export const routeToProjectsPage = () => {
   hashHistory.push('/projects');
+  window.scrollTo(0,0);
 }
 
 export const routeToTimerPage = () => {
   hashHistory.push('/');
+  window.scrollTo(0,0);
 } 
