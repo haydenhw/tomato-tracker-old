ae10e9526e95fc0bc2f44fbad9210b170011b00e
hayden7913 <hayden321@gmail.com>
2017-09-21 06:18:58 -0700

    made nav responsive

--- client/src/srcConfig/devSettings.js
+++ client/src/srcConfig/devSettings.js
@@ -1,5 +1,5 @@
 const productionMode = true;
-export const isOnboardingActive = productionMode ? false : false;
+export const isOnboardingActive = productionMode ? false : true;
 export const modalType = productionMode ? "WELCOME" : "ADD_PROJECT";
 const renderModal = false; 
 export const renderFormModal = productionMode ? false : (isOnboardingActive || renderModal);
