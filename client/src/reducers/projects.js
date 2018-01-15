5ef6833ef44b74b78f0ad557e79aef8b46afebb0
hayden7913 <hayden321@gmail.com>
2018-01-14 23:28:45 -0800

    deploying

--- client/src/reducers/projects.js
+++ client/src/reducers/projects.js
@@ -11,14 +11,14 @@ function tasks(state, action) {
         const newTasks = project.tasks.mapAndFindById('shortId', action.taskId, (task) => {
           return Object.assign({}, task, { _id: action.databaseId });
         });
-        
+
         return Object.assign({}, project, { tasks: newTasks });
       });
     case actions.DELETE_TASK_REQUEST:
       return state.mapAndFindById('shortId', action.projectId, (project) => {
         const deleteIndex = project.tasks.findIndex(task => task['shortId'] === action.taskId);
         const newTasks = project.tasks.sliceDelete(deleteIndex);
-        
+
         return Object.assign({}, project, { tasks: newTasks })
       });
     case actions.INCREMENT_TASK_TIME:
@@ -26,14 +26,13 @@ function tasks(state, action) {
         const newTasks = project.tasks.mapAndFindById('shortId', action.taskId, (task) => {
           return Object.assign({}, task, { recordedTime: task.recordedTime + 1 });
         });
-        
+
         return Object.assign({}, project, { tasks: newTasks });
       });
-
     case actions.EDIT_TASK_REQUEST:
       return state.mapAndFindById('shortId', action.projectId, (project) => {
         const newTasks = project.tasks.mapAndFindById('shortId', action.taskId, (task) => {
-          
+
           return Object.assign({}, task, action.toUpdate);
         });
         return Object.assign({}, project, { tasks: newTasks });
@@ -44,10 +43,10 @@ function tasks(state, action) {
 }
 
 const defaultState = {
-  hasFetched: false, 
-  isFetching: false, 
+  hasFetched: false,
+  isFetching: false,
   items: [],
-  queue: null 
+  queue: null
 }
 
 export function projects(state=defaultState, action) {
@@ -56,19 +55,19 @@ export function projects(state=defaultState, action) {
     return {
       ...state,
       activeContextMenuId: action.activeMenuId
-    } 
+    }
     case actions.TOGGLE_FETCHING:
     return {
       ...state,
       isFetching: !state.isFetching,
-    } 
+    }
     case actions.FETCH_PROJECTS_SUCCESS:
       return {
         ...state,
         items: action.projects,
-        hasFetched: true, 
+        hasFetched: true,
         isFetching: false
-      } 
+      }
     case actions.POST_PROJECT_REQUEST:
       return {
         ...state,
@@ -76,24 +75,24 @@ export function projects(state=defaultState, action) {
           ...state.items,
           action.project
         ]
-      }       
+      }
     case actions.POST_PROJECT_SUCCESS:
       return {
         ...state,
         items: state.items.mapAndFindById('shortId', action.projectId, (project) => {
           return Object.assign({}, project, { _id: action.databaseId })
         })
-      }       
+      }
     case actions.EDIT_PROJECT_NAME_REQUEST:
       return {
         ...state,
         items: state.items.mapAndFindById('shortId', action.projectId, (project) => {
           return Object.assign({}, project, { projectName: action.projectName })
         })
-      } 
+      }
     case actions.DELETE_PROJECT_REQUEST:
       const projectIndex = state.items.findIndex(project => project.shortId === action.project.shortId);
-        
+
       return {
         ...state,
         items: state.items.sliceDelete(projectIndex)
@@ -105,9 +104,9 @@ export function projects(state=defaultState, action) {
       }
     case actions.UPDATE_TASKS:
     case actions.POST_TASK_SUCCESS:
-    case actions.EDIT_TASK_REQUEST: 
-    case actions.DELETE_TASK_REQUEST: 
-    case actions.INCREMENT_TASK_TIME: 
+    case actions.EDIT_TASK_REQUEST:
+    case actions.DELETE_TASK_REQUEST:
+    case actions.INCREMENT_TASK_TIME:
       return {
         ...state,
         items: tasks(state.items, action)
