ff054b4847023754bc7e30a184299ae12c2fb9d8
hayden7913 <hayden321@gmail.com>
2020-12-12 13:08:24 -0800

    deletes unused server-files folder

--- server-files/config.js
+++
@@ -1,5 +0,0 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                      'mongodb+srv://ttuser:46869269a>@cluster0.xl3mg.mongodb.net/tomato-tracker?retryWrites=true&w=majority'
-                      'mongodb+srv://ttuser:46869269a>@cluster0.xl3mg.mongodb.net/tomato-tracker?retryWrites=true&w=majority';
-exports.PORT = process.env.PORT || 3006;
